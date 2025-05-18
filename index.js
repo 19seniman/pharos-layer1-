9dc7692aed8d8856b89b7b920a895420d9277200
19seniman <161638164+19seniman@users.noreply.github.com>
2025-05-18 23:31:38 +0700

    index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const logger = {
   banner: () => {
     console.log(`${colors.cyan}${colors.bold}`);
     console.log('-------------------------------------------------');
-    console.log(' 19Seniman From Insider - FREE PALESTINE ');
+    console.log(' 🍉🍉🍉 19Seniman From Insider - FREE PALESTINE 🍉🍉🍉 ');
     console.log('-------------------------------------------------');
     console.log(`${colors.reset}\n`);
   },
